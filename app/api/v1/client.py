bb77fcd1cec25d8c270532ae208e1997b1397354
xing-6464 <17621404964@163.com>
2024-07-01 22:15:16 +0800

    处理不同客户端注册的方案

--- app/api/v1/client.py
+++ app/api/v1/client.py
@@ -1,9 +1,20 @@
+from flask import request
 from app.libs.redprint import Redprint
 
 api = Redprint('client')
 
-api.route('register')
+api.route('register', methods=['POST'])
 def create_client():
+    data = request.json()
+    form = ClienForm(data=data)
+    if form.validate():
+        promise = {
+            ClientTypeEnum.USER_EMAIL: __register_user_by_email,
+        }
+
     # 注册
     # 参数 检验
+    pass
+
+def __register_user_by_email():
     pass
