4118dbb93a234acd9831b24b6c0a54869804c60a
xing-6464 <17621404964@163.com>
2024-07-01 21:13:32 +0800

    打开思维，创建自己的Redprint——红图

---
+++ app/api/v1/book.py
@@ -0,0 +1,8 @@
+from flask import Blueprint
+from app.libs.redprint import Redprint
+
+api = Redprint('book')
+
+@api.route('/get')
+def get_book():
+    return 'get book'
--- app/app.py
+++ app/app.py
@@ -2,6 +2,10 @@ from flask import Flask
 
 __author__ = 'xing'
 
+def register_blueprints(app):
+    from app.api.v1 import create_blueprint_v1
+    app.create_blueprint(create_blueprint_v1(), url_prefix='/api/v1')
+
 def create_app():
     app = Flask(__name__)
     app.config.from_object('app.config.setting')
